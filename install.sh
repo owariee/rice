11a4292c729383918bc838af8bf489af73fe6fc2
owariee <jgabrielsabatini@outlook.com>
2022-04-09 10:40:43 -0300

    x11 added

--- install.sh
+++ install.sh
@@ -2,7 +2,19 @@
 cd .config
 for d in */
 do
+  [[ -F ~/.config/$d ]] && rm ~/.config/$d
   [[ -d ~/.config/$d ]] && rm -rf ~/.config/$d
   ln -sf $(pwd)/$d ~/.config
   echo "Symlink created for ${d%/}!"
 done
+cd ..
+
+cd .local
+for d in */
+do
+  [[ -F ~/.local/$d ]] && rm ~/.local/$d
+  [[ -d ~/.local/$d ]] && rm -rf ~/.local/$d
+  ln -sf $(pwd)/$d ~/.local
+  echo "Symlink created for ${d%/}!"
+done
+cd ..
