3e6dd84c6444dcb0e5a014398a1ade9dc3179a0c
owariee <jgabrielsabatini@outlook.com>
2022-04-09 11:08:01 -0300

    fixed install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 cd .config
-for d in */
+for d in */ 
 do
-  [[ -F ~/.config/$d ]] && rm ~/.config/$d
-  [[ -d ~/.config/$d ]] && rm -rf ~/.config/$d
+  [[ -d ~/.local/${d%/} ]] && [[ ! -L ~/.config/${d%/} ]] && rm -rf ~/.config/$d
   ln -sf $(pwd)/$d ~/.config
   echo "Symlink created for ${d%/}!"
 done
@@ -12,8 +11,7 @@ cd ..
 cd .local
 for d in */
 do
-  [[ -F ~/.local/$d ]] && rm ~/.local/$d
-  [[ -d ~/.local/$d ]] && rm -rf ~/.local/$d
+  [[ -d ~/.local/${d%/} ]] && [[ ! -L ~/.local/${d%/} ]] && rm -rf ~/.local/$d
   ln -sf $(pwd)/$d ~/.local
   echo "Symlink created for ${d%/}!"
 done
